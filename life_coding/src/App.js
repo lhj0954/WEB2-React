a076464cb140744146887c29f8fee32fe1c36e88
lhj0954 <leehakjun111@naver.com>
2022-08-09 17:00:08 +0900

    이벤트 완료

--- life_coding/src/App.js
+++ life_coding/src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+function Header(props) {
+  return (
+    <header>
+      <h1>
+        <a
+          href="/"
+          onClick={(e) => {
+            e.preventDefault();
+            props.onChangeMode();
+          }}
+        >
+          {props.title}
+        </a>
+      </h1>
+    </header>
+  );
+}
+
+function Nav(props) {
+  const list = [];
+  props.topics.map((topic) => {
+    list.push(
+      <li key={topic.id}>
+        <a
+          id={topic.id}
+          href={"/read/" + topic.id}
+          onClick={(e) => {
+            e.preventDefault();
+            props.onChangeMode(e.target.id);
+          }}
+        >
+          {topic.title}
+        </a>
+      </li>
+    );
+    return null;
+  });
+
+  return (
+    <nav>
+      <ol>{list}</ol>
+    </nav>
+  );
+}
+
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  );
+}
 
 function App() {
+  const topics = [
+    { id: 1, title: "html", body: "html is ..." },
+    { id: 2, title: "css", body: "css is ..." },
+    { id: 3, title: "javascript", body: "javascript is ..." },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header
+        title="React"
+        onChangeMode={() => {
+          alert("header");
+        }}
+      ></Header>
+      <Nav
+        topics={topics}
+        onChangeMode={(id) => {
+          alert(id);
+        }}
+      ></Nav>
+      <Article title="Web" body="Hello, Web"></Article>
     </div>
   );
 }
